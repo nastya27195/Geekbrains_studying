6bc9439bcadfc2cfc5cecfa165331c45341d1413
Anastasiya <yua27195@gmail.com>
2022-11-18 00:04:15 +0300

    Add new Tasks

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine ("Введите целое цисло");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} = {square}");
--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 == num2 * num2)
+{
+    Console.WriteLine($"Да, число {num1} является квадратом числа {num2}");
+}
+else
+{
+    Console.WriteLine($"Нет, число {num1} не является квадратом числа {num2}");
+}
